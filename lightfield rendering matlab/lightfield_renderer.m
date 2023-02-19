1a526f0019be1b90d7af1a5e5fd094d5b9c4a4d1
IljaRukin <ilja.rukin@gmail.com>
2023-02-19 23:13:32 +0100

    finalize script

--- lightfield rendering matlab/lightfield_renderer.m
+++ lightfield rendering matlab/lightfield_renderer.m
@@ -1,6 +1,8 @@
+close all; clear; clc;
+
 %%%virtual image distance:
 dv = 10; %mm
-virtual_image = True;
+virtual_image = true;
 
 %%%lcd parameters (LS055R1SX03)
 res = [1440,2560]; %pixel
@@ -21,11 +23,16 @@ a = 3.5; %mm
 t1 = [1,0]*a; %mm
 t2 = [cos(2*pi/6),sin(2*pi/6)]*a; %mm
 
-%translation vectors - rectangular lenses
 %{
+%translation vectors - rectangular lenses
 t1 = [7,0]; %mm
 t2 = [0,5.4]; %mm
 %}
+
+%test alignment of t1 with x-axis
+if t1(2)>0
+    error('t1(2) has to be zero for algorithm to work !');
+end
 
 %pixel density along these vectors
 p1 = pix_density.*t1; %pixel/lens along vector
@@ -34,13 +41,13 @@ lp1 = sqrt(sum(p1.^2));
 lp2 = sqrt(sum(p2.^2));
 
 %%% distance lens-lcd
-if virtual_image==True
+if virtual_image==true
     d = dv*f/(dv+f);
-elseif virtual_image==False
+elseif virtual_image==false
     d = dv*f/(dv-f);
     dv = -dv;
 else
-    fprintf('wrong setup for virtual_image: please input True or False\n');
+    fprintf('wrong setup for virtual_image: please input true or false\n');
 end
 
 fprintf('adjust lens to distance d = %.3f mm\n',d);
@@ -48,14 +55,16 @@ offset_scaling = dv/d;
 
 %%%picture
 %load picture
-img = imread('Zebra.png');
-pic = sum(img,3); pic = pic/max(pic(:));
-shape = shape(pic);
+pic = imread('Zebra.png');
+img = sum(pic,3);
+img = img/max(img(:));
+img = double(img <0.8); %binary flip colors
+shape = size(img);
 
 %image scaling (constant virtual width wv)
 wv = 20; %mm
 pix_density_v = shape(2)/wv; %%pixel/mm
-s = 1/pix_density(1) * pix_density_v /2; %pixel/pixel
+s = 1/pix_density(2) * pix_density_v /2; %pixel/pixel
 
 %image shift (number of LCD pixel)
 x_shift = 900; %px
@@ -64,18 +73,16 @@ y_shift = 600; %px
 %%% pixel array:
 result = zeros(res(1),res(2));
 %for each pixel on lcd, find lens to pass through and pixel offset
-xx,yy = meshgrid(1:res(2),1:res(1)); %coordinate grid
+[xx,yy] = meshgrid(1:res(2),1:res(1)); %coordinate grid
 p2_lens = round(yy./p2(2));
 y_offset = yy - p2(2)*p2_lens;
 x_offset = xx - p2(1)*p2_lens;
-p1_lens = round(x_offset./p1(1));
-x_offset = x_offset - p1(1)*p1_lens;
-
+p1_lens = round(x_offset./p1(1)); %lens number in x-direction
+x_offset = x_offset - p1(1)*p1_lens; %coordinates inside lens in x-direction
 border = (x_offset.^2 + y_offset.^2) > ((abs(x_offset)-p2(1)).^2 + (abs(y_offset)-p2(2)).^2);
-p2_lens = p2_lens + sign(p2_lens)*border;
-y_offset = y_offset - sign(y_offset)*p2(2) * border;
-x_offset = x_offset - sign(x_offset)*p2(1) * border;
-grid = x_offset.^2 + y_offset.^2;
+p2_lens = p2_lens + sign(p2_lens).*border;
+y_offset = y_offset - sign(y_offset)*p2(2).*border;
+x_offset = x_offset - sign(x_offset)*p2(1).*border;
 
 %calculate virtual pixel x_0 from LCD pixel x_n (subtract offset from lens middle pixel and add offset scaled by image equation)
 %afterwards scaling with s (to compensate for the pixel density difference between the given image and the LCD)
@@ -84,16 +91,16 @@ y_pix = (yy - y_shift - y_offset + y_offset*offset_scaling)*s;
 
 %images outside of given image are not interpolated, since they are zero
 mask = ( (y_pix<(shape(1)-1)).*(y_pix>0) ) .* ( (x_pix<(shape(2)-1)).*(x_pix>0) );
-idx = where(mask);
+idx = find(mask);
 
 %interpolate color at virtual pixel
-result(idx) = interp2(pic.T,x_pix(idx),y_pix(idx));
+result(idx) = interp2(img,x_pix(idx),y_pix(idx));
 result = result/max(abs(result(:)));
-imsave(['zebra_dv_',num2str(dv),'_d_',num2str(d),'_f_',num2str(f),'.png'],result);
+imwrite(result,['zebra_dv_',num2str(dv),'_d_',num2str(d),'_f_',num2str(f),'.png']);
 
 % display results
 figure(1); title('original'); imshow(pic);
-figure(2); title('grid'); imshow(grid);
-figure(3); title('mapping'); imshow(sqrt( x_pix.^2 + y_pix.^2 ));
-figure(4); title('coverage'); imshow(mask.astype(int));
-figure(5); title('rendered'); imshow(result);
+figure(2); title('grid'); imagesc(sqrt(x_offset.^2 + y_offset.^2));
+figure(3); title('mapping'); imagesc(sqrt( x_pix.^2 + y_pix.^2 ));
+figure(4); title('coverage'); imagesc(mask);
+figure(5); title('rendered'); imagesc(result);
